1fa48945baf82110edd31cbd6a0d2f3bd533ba4c
madx <madx+github@yapok.org>
2012-12-20 01:29:56 +0100

    Make the SlideShow class visible

--- assets/prezto.js
+++ assets/prezto.js
@@ -7,74 +7,73 @@
  */
 
 var Prezto = function(window, $) {
-
-  var SlideShow = function() {
-    this.slides  = $('body > div');
-    this.current = 0;
-    this.last    = this.slides.size() - 1;
-  };
-
-  SlideShow.prototype.start = function() {
-    var self = this;
-    $(window).bind('keydown', function (ev) { self.keyDown(ev);          });
-    $(window).bind('resize',  function (ev) { self.jumpTo(self.current); });
-
-    this.slides.each(function(i, e) {
-      $(e).hide();
-    });
-
-    this.jumpTo(this.current);
-  };
-
-  SlideShow.prototype.fold = function() {
-    this.slides.each(function(i, e) {
-      $(e).hide();
-    });
-
-    this.jumpTo(this.current);
-  };
-
-  SlideShow.prototype.expand = function() {
-    this.slides.each(function(i, e) {
-      $(e).show();
-    });
-  };
-
-  SlideShow.prototype.jumpTo = function(slideId) {
-    $(this.slides[this.current]).hide();
-    this.current = slideId;
-    $(this.slides[slideId]).show();
-  };
-
-  SlideShow.prototype.next = function () {
-    if (this.current == this.last) return;
-    this.jumpTo(this.current + 1);
-  };
-
-  SlideShow.prototype.prev = function () {
-    if (this.current == 0) return;
-    this.jumpTo(this.current - 1);
-  };
-
-  SlideShow.prototype.home = function () {
-    this.jumpTo(0);
-  };
-
-  SlideShow.prototype.keyDown = function (ev) {
-    var handlers = {
-      32: this.next, // space
-      39: this.next, // right
-      37: this.prev, // left
-      36: this.home  // home
-    }
-
-    if (ev.which in handlers) {
-      handlers[ev.which].call(this);
-      ev.preventDefault();
-    }
-  };
-
-  window.slideShow = new SlideShow();
+  window.slideShow = new Prezto.SlideShow();
   window.slideShow.start();
+};
+
+Prezto.SlideShow = function() {
+  this.slides  = $('body > div');
+  this.current = 0;
+  this.last    = this.slides.size() - 1;
+};
+
+Prezto.SlideShow.prototype.start = function() {
+  var self = this;
+  $(window).bind('keydown', function (ev) { self.keyDown(ev);          });
+  $(window).bind('resize',  function (ev) { self.jumpTo(self.current); });
+
+  this.slides.each(function(i, e) {
+    $(e).hide();
+  });
+
+  this.jumpTo(this.current);
+};
+
+Prezto.SlideShow.prototype.fold = function() {
+  this.slides.each(function(i, e) {
+    $(e).hide();
+  });
+
+  this.jumpTo(this.current);
+};
+
+Prezto.SlideShow.prototype.expand = function() {
+  this.slides.each(function(i, e) {
+    $(e).show();
+  });
+};
+
+Prezto.SlideShow.prototype.jumpTo = function(slideId) {
+  $(this.slides[this.current]).hide();
+  this.current = slideId;
+  $(this.slides[slideId]).show();
+};
+
+Prezto.SlideShow.prototype.next = function () {
+  if (this.current == this.last) return;
+  this.jumpTo(this.current + 1);
+};
+
+Prezto.SlideShow.prototype.prev = function () {
+  if (this.current == 0) return;
+  this.jumpTo(this.current - 1);
+};
+
+Prezto.SlideShow.prototype.home = function () {
+  this.jumpTo(0);
+};
+
+Prezto.SlideShow.prototype.keyDown = function (ev) {
+  var handlers = {
+    32: this.next, // space
+    39: this.next, // right
+    37: this.prev, // left
+    36: this.home  // home
+  }
+
+  if (ev.which in handlers) {
+    handlers[ev.which].call(this);
+    ev.preventDefault();
+  }
 };
 
